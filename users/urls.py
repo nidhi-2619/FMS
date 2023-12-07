5a8af190d2aaccf6b6d4357608825afd88526f15
nidhi-2619 <91557785+nidhi-2619@users.noreply.github.com>
2023-12-07 23:10:03 +0530

    feat(user): custom serializers and views added

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 """URL mappings for the user API."""
 from django.urls import path
-from user import views
+from users import views
 
 
 app_name = 'user'
--- users/views.py
+++ users/views.py
@@ -35,3 +35,5 @@ class ManageUserView(generics.RetrieveUpdateAPIView):
     def get_object(self):
         """Retrieve and return the authenticated user."""
         return self.request.users
+
+
